a8dd4fc3d71b5f42f14fd9006fdd9af0d34e6828
i-agourram <302633165@student.rocmondriaan.nl>
2023-09-18 14:26:09 +0200

    Opdracht 6 gemaakt

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
+session_start();
+if (!isset($_SESSION['cijfers'])) {
+    $_SESSION['cijfers'] = array();
+}
+
+function berekenGemmidelde($array) {
+    if (count($array) === 0) {
+        return 0;
+    }
+    return array_sum($array) / count($array);
+}
 
 
 
+if (isset($_POST['cijfer'])) {
+    $ingevoerdCijfer = ($_POST['cijfer']);
 
+    if ($ingevoerdCijfer >= 1.0 && $ingevoerdCijfer <= 10.0) {
+        $_SESSION['cijfers'] [] = $ingevoerdCijfer;
+    }
+}
+
+$gemiddelde = berekenGemmidelde($_SESSION['cijfers']);
 ?>
 
 <!doctype html>
@@ -15,6 +34,28 @@
     <title>Document</title>
 </head>
 <body>
+
+<form method="POST">
+    Voer een cijfer in (tussen 1.0 en 10.0)
+
+    <input type="text" name="cijfer">
+    <br>
+    <input type="submit" value="Toevoegen">
+</form>
+
+
+<?php
+if (!empty($_SESSION['cijfers'])) {
+    echo "<p> ingevoerde cijfers: " . implode(", ", $_SESSION['cijfers']) . "</p>";
+    echo "<p> gemmidelde: " . number_format($gemiddelde, 2) . "</p>";
+} else {
+    echo "<p> Voer een geldig cijfer in tussen de 1.0 en 10.0! </p>";
+}
+
+
+
+?>
+
 
 </body>
 </html>
